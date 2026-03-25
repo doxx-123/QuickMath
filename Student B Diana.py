d5f44b6866e8fcb2ebaa300088821e664d3e3f01
dianaq-prog <dianaq@bu.edu>
2026-03-25 19:54:29 -0400

    Student B Session 3

--- Student B Diana.py
+++ Student B Diana.py
@@ -11,25 +11,35 @@ all timing logic."""
 
 import time
 
-def timeAndResponse(question):
-    """Tracks the time taken to answer the problem, and returns the time taken and user response."""
+def timeResponseValidity(question):
+    """Tracks the time taken to answer the problem and validates the answer is numeric.
+    Returns the time taken and the valid user response."""
+    isNumeric = False
+    attempts = 0
+    validCharacters = str([1,2,3,4,5,6,7,8,9,0]) # all answers will be in integer format
+
     startTime = time.time()
-    userResponse = input(question)
+    while isNumeric == False: # loops the question until the user enters a valid response
+        userResponse = input(question)
+        isNumeric = True
+        for character in userResponse:
+            if character not in validCharacters:
+                isNumeric = False
     endTime = time.time()
-    elapsedTime = round(endTime - startTime, 4) # time elapsed in seconds rounded to 4 decimal places
+    elapsedTime = round(endTime - startTime, 3) # time elapsed in seconds rounded to 4 decimal places
 
     return elapsedTime, userResponse
 
-def numericValidation(userResponse):
-    """Validates that the user responded with a numeric input. Returns a boolean value"""
-    validCharacters = str([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # all answers will be in integer format
-    isNumeric = True
-    for character in userResponse:
-        if character not in validCharacters:
-            isNumeric = False
-    return isNumeric
-
-def answerChecker(userResponse, correctAns):
-    """Checks if the user responded with the correct answer and returns a boolean"""
-    isCorrect = (userResponse == correctAns) #5 for correct, 2 for bonus speed
-    return isCorrect
+def answerChecker(userResponse, correctAns, elapsedTime):
+    """Checks if the user responded with the correct answer and awards points accordingly.
+    User recieves 5 points for a correct answer, and an additonal 2 for a speedy anser (<=5 seconds).
+    Returns a boolean for correctness, and points awarded."""
+    isCorrect = (userResponse == correctAns)
+    if isCorrect == True: # 5 for correct, 2 for bonus speed
+        if elapsedTime <= 5:
+            pointsAwarded = 5 + 2
+        else:
+            pointsAwarded = 5
+    else:
+        pointsAwarded = 0
+    return isCorrect, pointsAwarded
