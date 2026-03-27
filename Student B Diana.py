436579e4b6c5b8201c990eb0a025a26423899bcd
dianaq-prog <dianaq@bu.edu>
2026-03-27 14:53:23 -0400

    Student B Session 4

--- Student B Diana.py
+++ Student B Diana.py
@@ -1,3 +1,8 @@
+"""
+Student B: Timing & Validation
+Author: Diana Quach
+Date: 2026-03-27
+"""
 """Student B: Timing & Validation
 - Track time per problem: record time.time() before displaying the question, and again after the student
 answers; compute elapsed time
@@ -26,13 +31,13 @@ def timeResponseValidity(question):
             if character not in validCharacters:
                 isNumeric = False
     endTime = time.time()
-    elapsedTime = round(endTime - startTime, 3) # time elapsed in seconds rounded to 4 decimal places
+    elapsedTime = round(endTime - startTime, 2) # time elapsed in seconds rounded to 2 decimal places
 
     return elapsedTime, userResponse
 
 def answerChecker(userResponse, correctAns, elapsedTime):
     """Checks if the user responded with the correct answer and awards points accordingly.
-    User recieves 5 points for a correct answer, and an additonal 2 for a speedy anser (<=5 seconds).
+    User receives 5 points for a correct answer, and an additional 2 for a speedy answer (<=5 seconds).
     Returns a boolean for correctness, and points awarded."""
     isCorrect = (userResponse == correctAns)
     if isCorrect == True: # 5 for correct, 2 for bonus speed
@@ -43,3 +48,32 @@ def answerChecker(userResponse, correctAns, elapsedTime):
     else:
         pointsAwarded = 0
     return isCorrect, pointsAwarded
+"""- For Speed Mode: track total elapsed time across all problems and end the round when the limit is
+reached"""
+
+def speedMode():
+    """Provides multiple problems to be completed within 2 minutes"""
+    timeLimit = 10 # seconds = 2 minutes
+    startTime = time.time()
+    endTime = 0
+    points = 0
+
+    while (endTime - startTime) < timeLimit:
+        problem, answer = generateProblem(difficultyLevel) # relies on Student A
+        elapsedTime, userResponse = timeResponseValidity(problem)
+        endTime = time.time() # total time tracker
+        if (endTime - startTime) >= timeLimit: # total time elapsed exceeds time limit
+            overTime = round((endTime - startTime ) - timeLimit, 2)
+            print(f"TIME'S UP!\nAnswer NOT Accepted - overtime by {overTime} seconds")
+            print("Total Points:", points)
+            return points
+        else:
+            correct, pointsAwarded = answerChecker(userResponse, answer, elapsedTime)
+            points += pointsAwarded
+            if correct == True:
+                print(f"CORRECT! +{pointsAwarded}")
+            if correct == False:
+                print("INCORRECT!")
+    print("TIME'S UP")
+    print("Total Points:", points)
+    return points
